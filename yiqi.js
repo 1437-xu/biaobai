ed7c7eaed7f2c19d6934f1f8155d016026d37f0e
1437-xu <81420588+1437-xu@users.noreply.github.com>
2021-03-26 20:09:24 +0800

    Add files via upload

---
+++ yiqi.js
@@ -0,0 +1,30 @@
+//Java Script Document
+var Dianji=0;
+window.onload=function(){
+                var buhao=document.getElementByld("buhao");
+                var hao=document.getElementByld("hao");
+                buhao.onclick=function(){
+                                Dianji++;
+                                if(Dianji==1){
+                                                                     alert("你再考虑一下呗");
+                  }else if(Dianji==2){
+                                                                     alert("你是我见过的最美丽善可爱的女孩");
+                  }else if(Dianji==3){
+                                                                     alert("爱你");
+                  }else if(Dianji==4){
+                                                                     alert("家务我帮你干");
+                  }else if(Dianji==5){
+                                                                     alert("不藏私房钱");
+                  }else if(Dianji==6){
+                                                                     alert("房子写你名");
+                  }else if(Dianji==7){
+                                                                     alert("工资全上交");
+                  }else if(Dianji==8){
+                                                                     alert("我会学着怎么做好一个男友");
+                                                                     Dianji=1;
+                  }
+
+                }
+                hao.onclick=function(){
+                                alert(你终于同意了！");
+                }
